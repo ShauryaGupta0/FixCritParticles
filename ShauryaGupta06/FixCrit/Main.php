436105bfacd8d3ebeb0208ccb98c507fa923dcad
ShauryaGupta0 <168058242+ShauryaGupta0@users.noreply.github.com>
2024-05-16 09:33:24 +0530

    Update Main.php

--- ShauryaGupta06/FixCrit/Main.php
+++ ShauryaGupta06/FixCrit/Main.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace ShauryaGupta06\FixCrit;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ResourcePackStackPacket;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    const CHEMISTRY_PACK_ID = "0fba4063-dba1-4281-9b89-ff9390653530";
+
+    public function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDataPacketSend(DataPacketSendEvent $event){
+        $packets = $event->getPackets();
+        foreach ($packets as $key => $pk) {
+            if ($pk instanceof ResourcePackStackPacket) {
+                $packStack = $pk->resourcePackStack;
+                foreach ($packStack as $i => $resourcePack) {
+                    if ($resourcePack->getPackId() === self::CHEMISTRY_PACK_ID) {
+                        unset($packStack[$i]);
+                    }
+                }
+                $pk->resourcePackStack = array_values($packStack);
+                $packets[$key] = $pk;
+            }
+        }
+        $event->setPackets($packets);
+    }
+}
